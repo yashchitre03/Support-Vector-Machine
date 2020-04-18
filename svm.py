8efc13e769d2b30c58395f3a21e29fbb835907db
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-04-18 17:27:26 -0500

    Add desired outputs

--- svm.py
+++ svm.py
@@ -1,8 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 18 16:51:52 2020
+import cvxopt
+import numpy as np
+import matplotlib.pyplot as plt
 
-@author: yashc
-"""
+np.random.seed(100)
+
+samples = 100
+x = np.random.uniform(0, 1, (samples,2))
+
+d = np.empty((samples), int)
+
+for i in range(samples):
+    d[i] = 1 if x[i][1] < (1/5)*np.sin(10*x[i][0])+0.3 or (x[i][1]-0.8)**2 + (x[i][0]-0.5)**2 < (0.15)**2 else -1
+
+
 
 
